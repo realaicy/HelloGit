83c8fc6d0e069fcdafde39ff19f5e44752f4de96
REALAICY <REALAICY@HOMENB>
2012-02-29 08:48:42 +0800

    testgit v2

--- src/com/realaicy/test/git/TestGit.java
+++ src/com/realaicy/test/git/TestGit.java
@@ -5,7 +5,7 @@ package com.realaicy.test.git;
 
 /**
  * @author REALAICY
- *
+ * 
  */
 public class TestGit {
 
@@ -13,7 +13,8 @@ public class TestGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Hello Git, I am coming.");
 
 	}
 
