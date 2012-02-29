829d46a3509b3bc2dd370c2dfec2735c71f78f27
REALAICY <REALAICY@HOMENB>
2012-02-29 08:49:11 +0800

    v3

--- src/com/realaicy/test/git/TestGit.java
+++ src/com/realaicy/test/git/TestGit.java
@@ -14,7 +14,7 @@ public class TestGit {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Hello Git, I am coming.");
+		System.out.println("Hello Git, I am coming. Haha, My name is Xudong Liu");
 
 	}
 
