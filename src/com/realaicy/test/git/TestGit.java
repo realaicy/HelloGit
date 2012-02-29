ef24f1ab67be4012dcaf12485f9a789e84e7551d
REALAICY <REALAICY@HOMENB>
2012-02-29 08:47:59 +0800

    TestGit.java v1

---
+++ src/com/realaicy/test/git/TestGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.realaicy.test.git;
+
+/**
+ * @author REALAICY
+ *
+ */
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
